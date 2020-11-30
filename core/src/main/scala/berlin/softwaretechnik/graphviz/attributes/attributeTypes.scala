422e04a59625741451b2f69aa0372f43397c4003
Felix Leipold <felix.leipold@gmail.com>
2020-11-30 19:37:34 +0100

    Fixing some attribute types.

--- core/src/main/scala/berlin/softwaretechnik/graphviz/attributes/attributeTypes.scala
+++ core/src/main/scala/berlin/softwaretechnik/graphviz/attributes/attributeTypes.scala
@@ -4,25 +4,45 @@ package berlin.softwaretechnik.graphviz.attributes
 sealed trait ArrowType {}
 
 object ArrowType {
+
   case object normal extends ArrowType
+
   case object inv extends ArrowType
+
   case object dot extends ArrowType
+
   case object invdot extends ArrowType
+
   case object odot extends ArrowType
+
   case object invodot extends ArrowType
+
   case object none extends ArrowType
+
   case object tee extends ArrowType
+
   case object empty extends ArrowType
+
   case object invempty extends ArrowType
+
   case object diamond extends ArrowType
+
   case object odiamond extends ArrowType
+
   case object ediamond extends ArrowType
+
   case object crow extends ArrowType
+
   case object box extends ArrowType
+
   case object obox extends ArrowType
+
   case object open extends ArrowType
+
   case object halfopen extends ArrowType
+
   case object vee extends ArrowType
+
 }
 
 sealed trait ColorOrList {}
@@ -33,7 +53,14 @@ case class Color(rep: String) extends ColorOrList {
 
 case class ColorList(Seq: Color) extends ColorOrList
 
-sealed trait Rect {}
+/**
+ * The rectangle llx,lly,urx,ury gives the coordinates, in
+ * points, of the lower-left corner (llx,lly) and the
+ * upper-right corner (urx,ury).
+ */
+case class Rect(llx: Double, lly: Double, urx: Double, ury: Double) {
+  override def toString: String = s"$llx,$lly,$urx,$ury"
+}
 
 sealed trait Point {}
 
